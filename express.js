e2c39084577a1260ac561fd9bc359520f873114c
Chikka Rishikesh <chikkarishi9@gmail.com>
2025-01-13 21:59:04 +0530

    A basic dynamic routing system is implemented

--- express.js
+++ express.js
@@ -1,8 +1,34 @@
 const http = require('http');
 
+const routes = [{
+	status_code: 404,
+	content_type: 'text/plain',
+	data: "page not found"
+}];
+
+function routes_assigner(url, status_code, content_type, data) {
+	routes.unshift({
+		path: url,
+		status_code: status_code,
+		content_type: content_type,
+		data: data
+	});
+}
+
+function route_object_returner(route, url) {
+	for(const element of route) {
+		if(element.path === url) return element;
+	}
+	return route[route.length -1];
+}
+
+routes_assigner('/', 200, 'text/plain', 'hello world');
+
 let handler = (req, res) => {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end("hello world");
+	const route = route_object_returner(routes, req.url);
+
+	res.writeHead(route.status_code, {'Content-Type': route.content_type});
+	res.end(route.data);
 }
 
 const server = http.createServer(handler);
